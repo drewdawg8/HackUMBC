0b8f27131b3fe4223d8afff18ca7d131da3e43dd
drewdawg8 <dstewa17@masonlive.gmu.edu>
2018-04-28 22:36:16 -0800

    upload and success redirect

--- web/main.py
+++ web/main.py
@@ -1,52 +1,33 @@
-import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = './'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = './'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-from flask import send_from_directory
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
-
-@app.route('/success/')
+@app.route("/success", methods = ['POST','GET'])
 def success():
-    return 'success'
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+        action = request.form['name']
+        print(action)
+        if action == 'yes':
+            return redirect('/')
+
+
+
+@app.route("/", methods = ['POST','GET'])
+def index():
+    if request.method == 'POST':
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('success'))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template("success.html")
+    return render_template("index.html")
+
+@app.route("/hello/<string:name>/")
+def hello(name):
+    return render_template(
+        'index.html',name=name)
 
 if __name__ == "__main__":
     app.run()
