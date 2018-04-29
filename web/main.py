5399703ae497fc9f7f17ca3e8e997458a9fb7e5c
drewdawg8 <dstewa17@masonlive.gmu.edu>
2018-04-28 23:43:07 -0800

    added parse_text

--- web/main.py
+++ web/main.py
@@ -1,18 +1,25 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 from werkzeug.utils import secure_filename
 import os
+from parse import parse_text
+filename = "hello"
 
 app = Flask(__name__)
 UPLOAD_FOLDER = './'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-@app.route("/success", methods = ['POST','GET'])
-def success():
+@app.route("/success/<name>", methods = ['POST','GET'])
+def success(name):
     if request.method == 'POST':
-        action = request.form['name']
-        print(action)
+        action = request.form['action']
         if action == 'yes':
+            return redirect(url_for('text',filename = name))
+        else: #action == 'no':
             return redirect('/')
+    return render_template("success.html")
 
+@app.route("/text/<filename>")
+def text(filename):
+    return parse_text(filename)
 
 
 @app.route("/", methods = ['POST','GET'])
@@ -21,13 +28,11 @@ def index():
         file = request.files['file']
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return render_template("success.html")
+        #return render_template("success.html")
+        return redirect(url_for('success',name = filename))
     return render_template("index.html")
 
-@app.route("/hello/<string:name>/")
-def hello(name):
-    return render_template(
-        'index.html',name=name)
+
 
 if __name__ == "__main__":
     app.run()
