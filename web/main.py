1b1013687dcdca8cec3063bd699ac6f78f157f2b
Ubuntu <ubuntu@ip-172-31-84-131.ec2.internal>
2018-04-30 04:24:59 +0000

    fixed html

--- web/main.py
+++ web/main.py
@@ -13,6 +13,7 @@ def success(name):
         action = request.form['action']
         if action == 'yes':
             parse_text(name)
+	    os.remove(name)
             return redirect(url_for('text',filename = name))
         else:
             return redirect('/')
@@ -39,4 +40,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0",port=80)
